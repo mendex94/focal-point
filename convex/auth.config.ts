bc37f2aef6507720c33834b7e40516dba92c3226
Felipe Couto <lfelipecmendes@icloud.com>
2024-09-19 17:19:50 -0300

    add clerk authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  providers: [
+    {
+      domain: 'https://model-zebra-72.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+}
---
+++ src/app/_components/Providers.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { PropsWithChildren } from 'react'
+import { ClerkProvider, useAuth } from '@clerk/nextjs'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { ConvexReactClient } from 'convex/react'
+
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL as string,
+)
+
+export default function Providers({ children }: PropsWithChildren) {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  )
+}
