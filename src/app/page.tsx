11f0c113e9b40d1fe4442be072a860b4683c319d
Felipe Couto <lfelipecmendes@icloud.com>
2024-09-19 22:03:06 -0300

    feat: add reusable input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from './_components/Button'
+import { Checkbox } from './_components/Checkbox'
+
 export default function Home() {
   return (
     <div>
-      <main>Hello World</main>
+      <main>
+        Hello World
+        <Button>Primary Button</Button>
+        <Button variant="secondary">Secondary Button</Button>
+        <Button variant="danger">Danger Button</Button>
+        <Checkbox type="submit" />
+      </main>
     </div>
   )
 }
