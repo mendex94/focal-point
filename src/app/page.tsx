086fb771bf6e513bd18d6401713f950099a9a497
Felipe Couto <lfelipecmendes@icloud.com>
2024-09-21 12:59:33 -0300

    feat: add core components to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import { Button } from '@/components/Button'
-import { Checkbox } from '@/components/Checkbox'
-import { Input } from '@/components/Input'
+import { TasksContainer } from '@/components/tasks-container'
+import { CreateTask } from '@/components/create-task'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        Hello World
-        <Button>Primary Button</Button>
-        <Button variant="secondary">Secondary Button</Button>
-        <Button variant="danger">Danger Button</Button>
-        <Checkbox type="submit" />
-        <Input placeholder="My custom Input component..." />
-      </main>
-    </div>
+    <main className={styles.container}>
+      <TasksContainer />
+      <CreateTask />
+    </main>
   )
 }
