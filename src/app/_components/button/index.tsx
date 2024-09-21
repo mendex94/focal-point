bf94a4c5c2442812cb71a2da1e5494e61c47260d
Felipe Couto <lfelipecmendes@icloud.com>
2024-09-21 13:03:23 -0300

    feat: add reusable button component

---
+++ src/app/_components/button/index.tsx
@@ -0,0 +1,16 @@
+import { ButtonHTMLAttributes, forwardRef } from 'react'
+import styles from './button.module.scss'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'danger'
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant = 'primary', ...props }, ref) => {
+    const classNames = `${styles.button} ${styles[variant]}`
+
+    return <button {...props} ref={ref} className={classNames} />
+  },
+)
+
+Button.displayName = 'Button'
