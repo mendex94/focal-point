5fe3652b40aca366fdf4447762700796fcb2938a
Felipe Couto <lfelipecmendes@icloud.com>
2024-09-21 13:03:05 -0300

    feat: reusable checkbox

---
+++ src/app/_components/checkbox/index.tsx
@@ -0,0 +1,27 @@
+import { forwardRef, ComponentPropsWithoutRef, ElementRef } from 'react'
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import { Check } from '@phosphor-icons/react/dist/ssr'
+import styles from './checkbox.module.scss'
+
+export type CheckboxProps = ComponentPropsWithoutRef<
+  typeof CheckboxPrimitive.Root
+>
+
+export const Checkbox = forwardRef<
+  ElementRef<typeof CheckboxPrimitive.Root>,
+  CheckboxProps
+>(({ ...props }, forwardedRef) => {
+  return (
+    <CheckboxPrimitive.Root
+      ref={forwardedRef}
+      {...props}
+      className={styles.root}
+    >
+      <CheckboxPrimitive.Indicator className={styles.indicator} asChild>
+        <Check weight="bold" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  )
+})
+
+Checkbox.displayName = 'Checkbox'
